063e18e3ba24d071671df14ff69e1833a90a00b0
CallMeSlick <caescobar7@gmail.com>
2026-07-27 14:46:44 -0400

    Create spotify-token.js

---
+++ api/spotify-token.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+    const client_id = process.env.SPOTIFY_CLIENT_ID;
+    const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64')
+        },
+        body: 'grant_type=client_credentials'
+    });
+
+    const data = await response.json();
+    res.status(200).json({ token: data.access_token });
+}
